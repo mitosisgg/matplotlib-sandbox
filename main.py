8a656e3040882b4737186d81dbcc1d5b674c011c
Christian Lee <christianlee@sandiego.edu>
2024-01-02 21:16:10 -0800

    scatter plot

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 
+# read data
 stock_df = pd.read_csv('stock_data.csv')
+daily_return_df = pd.read_csv('stocks_daily_returns.csv')
 
+# line plots
 stock_df.plot(x = 'Date', y = 'FB', label = 'FB',  figsize = (15, 10), linewidth = 3) 
 plt.ylabel('Price ($)')
 plt.xlabel('Date')
@@ -21,5 +24,13 @@ plt.ylabel('Price ($)')
 plt.xlabel('Date')
 plt.title('Stock Price')
 plt.grid()
+
+# scatter plot
+X = daily_return_df['FB']
+Y = daily_return_df['NFLX']
+plt.figure(figsize = (15, 10))
+plt.scatter(X, Y)
+plt.grid()
+
 
 plt.show()
