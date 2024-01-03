56f6b13467aed7531d6f4ed7ffdbec719ecf0336
Christian Lee <christianlee@sandiego.edu>
2024-01-02 21:35:11 -0800

    add histogram

--- main.py
+++ main.py
@@ -2,35 +2,69 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-# read data
-stock_df = pd.read_csv('stock_data.csv')
-daily_return_df = pd.read_csv('stocks_daily_returns.csv')
+def plot_line_charts():
+    # line plots
+    stock_df.plot(x = 'Date', y = 'FB', label = 'FB',  figsize = (15, 10), linewidth = 3) 
+    plt.ylabel('Price ($)')
+    plt.xlabel('Date')
+    plt.title('Stock Price')
+    plt.grid()
 
-# line plots
-stock_df.plot(x = 'Date', y = 'FB', label = 'FB',  figsize = (15, 10), linewidth = 3) 
-plt.ylabel('Price ($)')
-plt.xlabel('Date')
-plt.title('Stock Price')
-plt.grid()
+    stock_df.plot(x = 'Date', y = 'NFLX', label = 'NFLX',  figsize = (15, 10), linewidth = 3, color = 'red') 
+    plt.ylabel('Price ($)')
+    plt.xlabel('Date')
+    plt.title('Stock Price')
+    plt.grid()
 
-stock_df.plot(x = 'Date', y = 'NFLX', label = 'NFLX',  figsize = (15, 10), linewidth = 3, color = 'red') 
-plt.ylabel('Price ($)')
-plt.xlabel('Date')
-plt.title('Stock Price')
-plt.grid()
+    stock_df.plot(x = 'Date', y = 'TWTR', label = 'TWTR',  figsize = (15, 10), linewidth = 3, color = 'green') 
+    plt.ylabel('Price ($)')
+    plt.xlabel('Date')
+    plt.title('Stock Price')
+    plt.grid()
 
-stock_df.plot(x = 'Date', y = 'TWTR', label = 'TWTR',  figsize = (15, 10), linewidth = 3, color = 'green') 
-plt.ylabel('Price ($)')
-plt.xlabel('Date')
-plt.title('Stock Price')
-plt.grid()
+    plt.show()
 
-# scatter plot
-X = daily_return_df['FB']
-Y = daily_return_df['NFLX']
-plt.figure(figsize = (15, 10))
-plt.scatter(X, Y)
-plt.grid()
+def plot_scatter_plot():
+    # scatter plot
+    X = daily_return_df['FB']
+    Y = daily_return_df['NFLX']
+    plt.figure(figsize = (15, 10))
+    plt.scatter(X, Y)
+    plt.grid()
+    plt.show()
+
+def plot_pie_chart():
+    # pie chart
+    values = [20, 55, 5, 17, 3]
+    colors = ['g', 'r', 'y', 'b', 'm']
+    explode = [0, 0, 0, 0, 0.2]
+    labels = ['AAPL', 'GOOG', 'T', 'TSLA', 'AMZN']
+
+    plt.figure(figsize = (10, 10))
+    plt.pie(values, explode, labels, colors)
+    plt.title('STOCK PORTFOLIO')
+    plt.show()
+
+def plot_histogram():
+    mu = round(daily_return_df['FB'].mean(), 2)
+    sigma = round(daily_return_df['FB'].std(), 2)
+    num_bins = 40
+
+    plt.figure(figsize = (15,9))
+    plt.hist(daily_return_df['FB'], num_bins, facecolor = 'blue')
+    plt.grid()
+    plt.title('Histogram: mu = ' + str(mu) + ' sd = ' + str(sigma))
+    plt.show()
 
 
-plt.show()
+
+if __name__ == '__main__':
+    # read data
+    stock_df = pd.read_csv('stock_data.csv')
+    daily_return_df = pd.read_csv('stocks_daily_returns.csv')
+
+    # plot_line_charts()
+    # plot_scatter_plot()
+    # plot_pie_chart()
+    # plot_histogram()
+    
